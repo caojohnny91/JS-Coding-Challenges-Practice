2d8c006a546f0f43958f8c6b824fc1ec5ffecfce
caojohnny91 <168755794+caojohnny91@users.noreply.github.com>
2024-08-17 15:16:36 -0500

    complete challenge 4

--- JS-fundmentals-1.js
+++ JS-fundmentals-1.js
@@ -149,6 +149,11 @@ console.log("-------------------------------- Coding Challenge #4");
 // § To calculate 20% of a value, simply multiply it by 20/100 = 0.2 § Value X is between 50 and 300, if it's >= 50 && <= 300😉
 // GOOD LUCK😀
 
+const calcTip = function (bill) {
+  const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+  return console.log(`The bill was $${bill}, the tip was $${tip}, and the total was $${bill + tip}.`);
+};
 
+calcTip(275);
 
 //  --------------------------------------------------------------------------------- 8
