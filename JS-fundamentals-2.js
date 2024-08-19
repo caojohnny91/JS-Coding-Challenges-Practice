e2ac31541e1bb1394fc891b337ae5f2a079decdd
caojohnny91 <168755794+caojohnny91@users.noreply.github.com>
2024-08-19 11:21:46 -0500

    complete 4

--- JS-fundamentals-2.js
+++ JS-fundamentals-2.js
@@ -57,10 +57,26 @@ console.log("-------------------------------- Coding Challenge #2");
 // Your tasks:
 // 1. Write a function 'calcTip' that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100
 // 2. And now let's use arrays! So create an array 'bills' containing the test data below
-// 3. Creat ean array 'tips' containing the tip value for each bill, calculated from the function you created before
-// 4. Bonus: Create an array 'total' containing the total values, so the bill+tip Test data: 125, 555 and 44
+// 3. Create an array 'tips' containing the tip value for each bill, calculated from the function you created before
+// 4. Bonus: Create an array 'total' containing the total values, so the bill+tip
+// Test data: 125, 555 and 44
 // Hint: Remember that an array needs a value in each position, and that value can actually be the returned value of a function! So you can just call a function as array values (so don't store the tip values in separate variables first, but right in the new array) 😉
 // GOOD LUCK😀
+
+let bills = [125, 555, 44];
+
+const calcTipNew = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+
+console.log(calcTipNew(100)); // 15
+
+let tips = [calcTipNew(bills[0]), calcTipNew(bills[1]), calcTipNew(bills[2])];
+console.log(tips); // 18.75, 111, 8.8
+
+let totals = [bills[0] + calcTipNew(bills[0]), bills[1] + calcTipNew(bills[1]), bills[2] + calcTipNew(bills[2])];
+
+console.log(totals); // 143.75, 666, 52.8
 
 // --------------------------------------------------------------------------------- 10
 
@@ -89,10 +105,42 @@ console.log("-------------------------------- Coding Challenge #4");
 // Test data: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
 // Hints: Call ‘calcTip ‘in the loop and use the push method to add values to the
 // tips and totals arrays😉
-// Bonus:4. Bonus: Write a function 'calcAverage' which takes an array called 'arr' as an argument. This function calculates the average of all numbers in the given array. This is a difficult challenge (we haven't done this before)! Here is how to solve it:
+
+// Bonus:4.
+// Bonus: Write a function 'calcAverage' which takes an array called 'arr' as an argument. This function calculates the average of all numbers in the given array. This is a difficult challenge (we haven't done this before)! Here is how to solve it:
 // 4.1. First, you will need to add up all values in the array. To do the addition, start by creating a variable 'sum' that starts at 0. Then loop over the array using a for loop. In each iteration, add the current value to the 'sum' variable. This way, by the end of the loop, you have all values added together
 // 4.2. To calculate the average, divide the sum you calculated before by the length of the array (because that's the number of elements)
 // 4.3. Call the function with the 'totals' array
 // GOOD LUCK😀
+
+bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+
+tips = [];
+
+totals = [];
+
+const calcAmounts = function (bill) {
+  for (let i = 0; i < bill.length; i++) {
+    tips.push(calcTipNew(bill[i]));
+    totals.push(bill[i] + tips[i]);
+  }
+};
+
+calcAmounts(bills);
+console.log(tips); // [4.4, 44.25, 26.4, 88, 7.4, 15.75, 2, 220, 12.9, 7.8]
+console.log(totals); // [26.4, 339.25, 202.4, 528, 44.4, 120.75, 12, 1320, 98.9, 59.8] // 2751.90
+
+const calcArrayAverage = function (arr) {
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  console.log("sum:", sum);
+  return sum / arr.length;
+};
+
+console.log(calcArrayAverage(totals)); // 275.19
+// console.log(2751.9 / 10); // just to double check answer
 
 // --------------------------------------------------------------------------------- 12
